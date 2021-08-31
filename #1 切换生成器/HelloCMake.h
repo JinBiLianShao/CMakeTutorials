bdb6da0cc48c78be05fcae654f7d45aa46987db7
liangyusheng <liangyusheng@myself.com>
2021-08-31 16:10:02 +0800

    1.2 切换生成器

---
+++ #1 切换生成器/HelloCMake.h
@@ -0,0 +1,17 @@
+﻿// HelloCMake.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+
+namespace CM
+{
+    void hello_cmake()
+    {
+        std::cout << "Hello, CMake!\n";
+    }
+} // namespace CM
+
+
+// TODO: 在此处引用程序需要的其他标头。
