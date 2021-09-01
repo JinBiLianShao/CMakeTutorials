5b1e339b637d3753589fe06b437cdbda6f1ff0f4
liangyusheng <liangyusheng@myself.com>
2021-09-01 16:03:08 +0800

    1.10 使用控制流

---
+++ #9 使用控制流/ProgressBar.cpp
@@ -0,0 +1,35 @@
+﻿#include "ProgressBar.h"
+
+ProgressBar::ProgressBar()
+    // 25 spaces
+    : m_bar("[                                                  ]")
+    , m_shap("#")
+{
+}
+
+void ProgressBar::run()
+{
+    int i = 0;
+    std::string::size_type pos = 0;
+
+    while (true)
+    {
+        // 休眠 0 ~ 200 毫秒。
+        std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 100));
+        // 因为进度条的 '#' 只有 50 个，所以每一个 '#' 代表 2%。
+        if (pos++ % 2)
+        {
+            // 将 ' ' 替换为 '#'。
+            this->m_bar.replace(pos / 2, 1, "#");
+        }
+
+        // 输出进度条。
+        std::cout << '\r' << this->m_bar << " " << i << " %" << std::flush;
+
+        if (i++ == 100)
+        {
+            std::cout << "\ndone.\n";
+            break;
+        }
+    }
+}
---
+++ #9 使用控制流/download.cpp
@@ -0,0 +1,17 @@
+﻿#include "ProgressBar.h"
+#include "Sleep.h"
+#include "Printer.h"
+
+int main()
+{
+    ProgressBar bar;
+    Printer p;
+    p.info("=========  downloading  =========\n");
+
+    Sleep s;
+    s.for_(2000);
+
+    bar.run();
+
+    return 0;
+}
